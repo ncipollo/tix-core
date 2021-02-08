b0b84fa05d56e7e18ef20749f0a418e2170dc7a2
Nick Cipollo <njc1115@gmail.com>
2021-02-07 20:08:23 -0500

    Add github action and sanity test

---
+++ src/commonTest/kotlin/tixorg/SanityTest.kt
@@ -0,0 +1,13 @@
+package tixorg
+
+import kotlin.test.Test
+import kotlin.test.expect
+
+class SanityTest {
+    @Test
+    fun testSanity() {
+        expect(true) {
+            false
+        }
+    }
+}
