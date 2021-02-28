809593a8409895e89705c763964c0ab20867ec55
Nick Cipollo <njc1115@gmail.com>
2021-02-28 18:23:42 -0500

    Add paragraph parser test

--- src/commonTest/kotlin/org/tix/feature/plan/parse/nodeparser/ParagraphParserTest.kt
+++ src/commonTest/kotlin/org/tix/feature/plan/parse/nodeparser/ParagraphParserTest.kt
@@ -1,18 +1,27 @@
 package org.tix.feature.plan.parse.nodeparser
 
+import org.tix.model.ticket.body.*
 import kotlin.test.Test
+import kotlin.test.expect
 
 class ParagraphParserTest {
     private val parser = ParagraphParser(NodeParserMap())
 
     @Test
     fun parse() {
-//        val arguments = "text".toParserArguments().childArguments!!
-//        arguments.state.startTicket()
-//
-//        val results = parser.parse(arguments)
-//
-//        expect(listOf<BodySegment>(TextSegment("text"))) { arguments.state.currentTicket!!.body }
-//        expect(1) { results.nextIndex }
+        val arguments = "line1\nline2".toParserArguments()
+        arguments.state.startTicket()
+
+        val results = parser.parse(arguments)
+
+        val expectedSegment = ParagraphSegment(
+            listOf(
+                TextSegment("line1"),
+                LineBreakSegment,
+                TextSegment("line2")
+            ).toTicketBody()
+        )
+        expect(listOf<BodySegment>(expectedSegment)) { arguments.state.currentTicket!!.body }
+        expect(1) { results.nextIndex }
     }
 }
