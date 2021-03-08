a2071c2dcc380d64576376c93b3efaf32b192367
Nick Cipollo <njc1115@gmail.com>
2021-03-07 20:40:35 -0500

    Add ordered item tests

--- src/commonTest/kotlin/org/tix/feature/plan/parse/nodeparser/ListParserTest.kt
+++ src/commonTest/kotlin/org/tix/feature/plan/parse/nodeparser/ListParserTest.kt
@@ -75,4 +75,74 @@ class ListParserTest {
         }
         expect(1) { results.nextIndex }
     }
+
+    @Test
+    fun parse_ordered_singleLevel() {
+        val arguments = """
+            1. item1
+            2. item2
+        """.trimIndent().toParserArguments()
+        arguments.state.startTicket()
+
+        val results = parser.parse(arguments)
+
+        expectBody(arguments) {
+            orderedList {
+                orderedListItem(0, 1) { paragraph { text("item1") } }
+                linebreak()
+                orderedListItem(0, 2) { paragraph { text("item2") } }
+            }
+        }
+        expect(1) { results.nextIndex }
+    }
+
+    @Test
+    fun parse_ordered_nestedBullets() {
+        val arguments = """
+            1. item1
+                10) nested1
+                11) nested2
+            2. item2
+                1. nested3
+                2. nested4
+                    1. nested5
+                    2. nested6
+        """.trimIndent().toParserArguments()
+        arguments.state.startTicket()
+
+        val results = parser.parse(arguments)
+
+        expectBody(arguments) {
+            orderedList {
+                orderedListItem(0, 1) {
+                    paragraph { text("item1") }
+                    linebreak()
+                    orderedList(1) {
+                        orderedListItem(1, 10) { paragraph { text("nested1") } }
+                        linebreak()
+                        orderedListItem(1, 11) { paragraph { text("nested2") } }
+                    }
+                }
+                linebreak()
+                orderedListItem(0, 2) {
+                    paragraph { text("item2") }
+                    linebreak()
+                    orderedList(1) {
+                        orderedListItem(1, 1){ paragraph { text("nested3") } }
+                        linebreak()
+                        orderedListItem(1, 2) {
+                            paragraph { text("nested4") }
+                            linebreak()
+                            orderedList(2) {
+                                orderedListItem(2, 1) { paragraph { text("nested5") } }
+                                linebreak()
+                                orderedListItem(2, 2) { paragraph { text("nested6") } }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        expect(1) { results.nextIndex }
+    }
 }
