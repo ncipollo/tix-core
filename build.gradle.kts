8758c950aa3f6dda7185ca0ad2b70fbfa490f376
Nick Cipollo <njc1115@gmail.com>
2021-03-22 10:18:14 -0400

    Refactor native targets in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,22 +23,18 @@ kotlin {
             useJUnit()
         }
     }
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
 
-    nativeTarget.apply {
-        binaries {
-            sharedLib {
-                baseName = "core"
+    val nativeTargets = listOf(linuxX64(), macosX64(), mingwX64())
+    nativeTargets.forEach {
+        it.apply {
+            binaries {
+                sharedLib {
+                    baseName = "core"
+                }
             }
         }
     }
+
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -61,7 +57,23 @@ kotlin {
                 implementation(kotlin("test-junit"))
             }
         }
-        val nativeMain by getting
-        val nativeTest by getting
+
+        val linuxX64Main by getting
+        val linuxX64Test by getting
+
+        val mingwX64Main by getting
+        val mingwX64Test by getting
+
+        val macosX64Main by getting
+        val macosX64Test by getting
+
+        val desktopMain by creating {
+            dependsOn(commonMain)
+            listOf(linuxX64Main, mingwX64Main, macosX64Main).forEach { it.dependsOn(this) }
+        }
+        val desktopTest by creating {
+            dependsOn(commonMain)
+            listOf(linuxX64Test, mingwX64Test, macosX64Test).forEach { it.dependsOn(this) }
+        }
     }
 }
