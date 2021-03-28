1acd3a9c2ef9935d994bed3effb6c5232ea1c09c
Nick Cipollo <njc1115@gmail.com>
2021-03-28 16:43:02 -0400

    Cleanup gradle more

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("UNUSED_VARIABLE")
+
 plugins {
     val kotlinVersion = "1.4.31"
     kotlin("multiplatform") version kotlinVersion
@@ -24,16 +26,24 @@ kotlin {
         }
     }
 
-    val nativeTargets = listOf(linuxX64(), macosX64(), mingwX64())
-    nativeTargets.forEach {
-        it.apply {
-            binaries {
-                sharedLib {
-                    baseName = "core"
-                }
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+    nativeTarget.apply {
+        binaries {
+            sharedLib {
+                baseName = "core"
             }
         }
     }
+    linuxX64()
+    macosX64()
+    mingwX64()
 
     sourceSets {
         val commonMain by getting {
@@ -60,20 +70,11 @@ kotlin {
 
         val linuxX64Main by getting
         val linuxX64Test by getting
-
-        val mingwX64Main by getting
-        val mingwX64Test by getting
-
         val macosX64Main by getting
         val macosX64Test by getting
-
-        val desktopMain by creating {
-            dependsOn(commonMain)
-            listOf(linuxX64Main, mingwX64Main, macosX64Main).forEach { it.dependsOn(this) }
-        }
-        val desktopTest by creating {
-            dependsOn(commonMain)
-            listOf(linuxX64Test, mingwX64Test, macosX64Test).forEach { it.dependsOn(this) }
-        }
+        val mingwX64Main by getting
+        val mingwX64Test by getting
+        val nativeMain by getting
+        val nativeTest by getting
     }
 }
