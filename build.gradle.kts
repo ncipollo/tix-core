a1cf2d206786c91aac48c858c5a07aa1c1e719e0
Nick Cipollo <njc1115@gmail.com>
2023-04-26 20:23:15 -0400

    Add release matrix

--- build.gradle.kts
+++ build.gradle.kts
@@ -123,6 +123,22 @@ kotlin {
         }
     }
 
+    // Avoid publishing duplicate artifacts on CI
+    // (since we need to run in a matrix to publish different platforms).
+    val publicationsFromMainHost =
+        listOf(jvm(), js()).map { it.name } + "kotlinMultiplatform"
+
+    publishing {
+        publications {
+            matching { it.name in publicationsFromMainHost }.all {
+                val targetPublication = this@all
+                tasks.withType<AbstractPublishToMaven>()
+                    .matching { it.publication == targetPublication }
+                    .configureEach { onlyIf {  System.getenv("IS_MAIN_PUBLISHER") == "true" } }
+            }
+        }
+    }
+
     sourceSets {
         all {
             languageSettings.optIn("kotlinx.coroutines.ExperimentalCoroutinesApi")
@@ -130,10 +146,6 @@ kotlin {
             // This is required for Turbine
             languageSettings.optIn("kotlin.time.ExperimentalTime")
         }
-//        The following snippet causes the IDE to exception, we should test it again in the next IDEA version.
-//        kotlin.sourceSets.matching { it.name.endsWith("Test") }.configureEach {
-//            languageSettings.useExperimentalAnnotation("kotlin.time.ExperimentalTime")
-//        }
 
         val commonMain by getting {
             dependencies {
