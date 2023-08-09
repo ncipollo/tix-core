f4f8b0627ec9ba189b18c9941511288072216202
Nick Cipollo <njc1115@gmail.com>
2023-08-08 22:41:21 -0400

    Fix linux build errors (needed to opt in to api)

--- src/linuxX64Main/kotlin/org/tix/platform/PlatformEnv.kt
+++ src/linuxX64Main/kotlin/org/tix/platform/PlatformEnv.kt
@@ -1,8 +1,10 @@
 package org.tix.platform
 
+import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.toKString
 import platform.posix.getenv
 
+@OptIn(ExperimentalForeignApi::class)
 actual object PlatformEnv : Env {
     actual override operator fun get(name: String) = getenv(name)?.toKString() ?: ""
 }
--- src/linuxX64Main/kotlin/org/tix/platform/path/HomeDirectory.kt
+++ src/linuxX64Main/kotlin/org/tix/platform/path/HomeDirectory.kt
@@ -1,9 +1,11 @@
 package org.tix.platform.path
 
+import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.pointed
 import kotlinx.cinterop.toKString
 import platform.posix.getpwuid
 import platform.posix.getuid
 
+@OptIn(ExperimentalForeignApi::class)
 internal actual val homeDirectory: String
     get() = getpwuid(getuid())?.pointed?.pw_dir?.toKString() ?: noHomeError()
